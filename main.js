64ccc34b900f1675b4df8dc78ba1af073d6ceb28
Jev Forsberg <jev.forsberg@gmail.com>
2019-03-16 14:26:43 -0600

    Add JS Functionality

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 function userDogRename() {
+	
 	var userInput = document.getElementById("header__form__input--text").value;
-	document.getElementById("header__title--rename").innerHTML = userInput;
+	// I decided to add a little fun piece to the project.
+	// My dog's name is "Calvin," and if his name is typed in the renaming input, HIS photo will replace the other dog's photo also.
+	if (userInput === "Calvin") {
+		document.getElementById("header__title--rename").innerHTML = userInput;
+		document.getElementById("js_change_dog").src="images/Calvin.jpg"
+	} else {
+		document.getElementById("header__title--rename").innerHTML = userInput;
+	}
 }
